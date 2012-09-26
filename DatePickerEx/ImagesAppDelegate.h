86cfd343cf056b6d3de4c4b3fe49b61aa74ebfe3
APPLE <apple@APPLEs-Mac-mini-2.local>
2012-09-26 23:29:54 +0530

    Initial Commit

---
+++ DatePickerEx/ImagesAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ImagesAppDelegate.h
+//  DatePickerEx
+//
+//  Created by APPLE on 9/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ImagesViewController;
+
+@interface ImagesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ImagesViewController *viewController;
+
+@end
---
+++ DatePickerEx/ImagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesViewController.h
+//  DatePickerEx
+//
+//  Created by APPLE on 9/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesViewController : UIViewController
+
+@end
